7d1b5f4a471cb50a535b7156e37515b36c7d5884
zdamen <zdamen@umich.edu>
2023-04-07 01:44:43 -0400

    updated the port api is listening to

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const _ = require("lodash");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.post("/UpdateData", (req, res) => {
@@ -33,4 +34,5 @@ app.get('/GetPrices', (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("API Server is running..."));
+app.listen(process.env.PORT || 3000, () => console.log("API Server is running..."));
+
