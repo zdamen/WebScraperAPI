1c9cae03f4afe6aee975734775dd04c7aae96544
zdamen <zdamen@umich.edu>
2023-04-08 23:30:07 -0400

    added caching to improve speeds

--- main.js
+++ main.js
@@ -7,6 +7,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let cache = null; // Add a variable to store the cached data
+
+function readDataFromFile() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("Database.json", "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const jsonData = JSON.parse(data);
+        resolve(jsonData);
+      }
+    });
+  });
+}
+
+function loadData() {
+  return readDataFromFile()
+    .then((data) => {
+      cache = data;
+      return data;
+    })
+    .catch((err) => {
+      console.error(err);
+      return null;
+    });
+}
+
 app.post("/UpdateData", (req, res) => {
   let data = req.body;
   console.log(data);
@@ -17,22 +44,34 @@ app.post("/UpdateData", (req, res) => {
       res.status(500).json({ message: "Failed to update data" });
     } else {
       console.log("Data written to file");
+      loadData(); // Update the cache when the file is updated
       res.status(200).json({ message: "Data updated successfully" });
     }
   });
 });
 
-app.get('/GetPrices', (req, res) => {
-  fs.readFile('Database.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error reading Database.json');
-    } else {
-      const jsonData = JSON.parse(data);
-      res.json(jsonData);
-    }
-  });
+app.get("/GetPrices", (req, res) => {
+  if (cache) {
+    // Serve the data from the cache if it's available
+    res.json(cache);
+  } else {
+    // Load the data from the file and store it in the cache if it's not available
+    loadData()
+      .then((data) => {
+        if (data) {
+          res.json(data);
+        } else {
+          res.status(500).send("Error reading Database.json");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error reading Database.json");
+      });
+  }
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("API Server is running..."));
-
+app.listen(process.env.PORT || 3001, () => {
+  console.log("API Server is running...");
+  loadData(); // Load the data into the cache when the server starts
+});
